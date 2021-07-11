d5ba08f307f0c84774059056f5f6600d729a8bf7
Chiranjit Dey <chiranjitdey2000@gmail.com>
2021-07-11 18:00:59 +0530

    setting up

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+//express app
+const app = express();
+
+//register view engine
+app.set('viewengine', 'ejs')
+
+//listen on port
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    //res.send('<p>Homepage </p>')
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+    //res.send('<p>About page </p>')
+    res.sendFile('./views/about.html', {root: __dirname});
+});
+
+//redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+//404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname});
+})
