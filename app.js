a15f6f084682fe9fa8b694a8867b133b83e12f3b
Chiranjit Dey <chiranjitdey2000@gmail.com>
2021-07-11 21:06:15 +0530

    express setup| blog app

--- app.js
+++ app.js
@@ -4,27 +4,34 @@ const express = require('express');
 const app = express();
 
 //register view engine
-app.set('viewengine', 'ejs')
+app.set('view engine', 'ejs')
 
 //listen on port
 app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p>Homepage </p>')
-    res.sendFile('./views/index.html', {root: __dirname});
+    //res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index', {title: 'Homepage'});
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>About page </p>')
-    res.sendFile('./views/about.html', {root: __dirname});
+    //res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About page'});
 });
 
 //redirects
 app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+    res.redirect('about');
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create'});
 });
 
 //404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    //res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {title:'Error'});
 })
